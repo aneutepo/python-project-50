2299a0b649a11be68e5b6a6e0745ef52a71ba308
Anton <poverenosn@gmail.com>
2024-10-16 12:38:08 +0300

    add new configurate files and functions

--- hexlet-code/gendiff/generate_diff.py
+++ hexlet-code/gendiff/generate_diff.py
@@ -1,4 +1,42 @@
 import json
 
-ex = json.load(open('/Users/isk/python-project-50/hexlet-code/files/file1.json'))
-print(ex)
+
+# Функция открытия файлов и сортировка их содержимого
+def open_files(filepath1, filepath2):
+    f1 = json.load(open(filepath1))
+    f2 = json.load(open(filepath2))
+    f1 = dict(sorted(f1.items()))
+    f2 = dict(sorted(f2.items()))
+    return (f1, f2)
+
+
+def calculate_differences(file1, file2):
+    result = {}
+    deleted_list = []
+    different_referenses = []
+    k1 = file1.keys()
+    k2 = file2.keys()
+    new_points_f1 = k2-k1
+    for k1, v1 in file1.items():
+        if file2.get(k1, 0):
+            if file1[k1] == file2[k1]:
+                result[k1] = file2[k1]
+            else:
+                different_referenses.append(file2[k1])
+                result[f"- {k1}"] = file1[k1]
+                result[f"+ {k1}"] = file2[k1]
+        else:
+            deleted_list.append(k1)
+            result[f"- {k1}"] = file1[k1]
+
+        for i in new_points_f1:
+            result[f"+ {i}"] = file2[i]
+    result = dict(sorted(
+        result.items(), key=lambda item: item[0].lstrip('+- ')))
+    return result
+
+
+path1 = 'hexlet-code/files/file1.json'
+path2 = 'hexlet-code/files/file2.json'
+file1, file2 = open_files(path1, path2)
+print(calculate_differences(file1, file2))
