c07c0deebf161edc9804cf2b627482ac6f167cd3
Anton <poverenosn@gmail.com>
2025-02-22 17:12:37 +0300

    add new configurate files and functions

--- tests/test_calculate_differense.py
+++ tests/test_calculate_differense.py
@@ -1,4 +1,4 @@
-from gendiff.generate_diff import start_calculate
+from gendiff.generate_diff import generate_diff
 import os
 import json
 
@@ -7,7 +7,7 @@ def test_calculating():
     current_directory = os.path.dirname(__file__)
     path1 = os.path.join(current_directory, 'fixtures', "file1.json")
     path2 = os.path.join(current_directory, 'fixtures', "file2.json")
-    result = start_calculate(path1, path2)
+    result = generate_diff(path1, path2)
     etalon_path = os.path.join(
         current_directory,
         'fixtures',
@@ -22,7 +22,7 @@ def test_calculating1():
     current_directory = os.path.dirname(__file__)
     path1 = os.path.join(current_directory, 'fixtures', "file1.json")
     path2 = os.path.join(current_directory, 'fixtures', "file2.json")
-    result = start_calculate(path1, path2, format_type='plain')
+    result = generate_diff(path1, path2, format_type='plain')
     etalon_path = os.path.join(current_directory, 'fixtures', 'etalon1.txt')
     with open(etalon_path, 'r') as etalon_file:
         expected_result = etalon_file.read()
@@ -33,7 +33,7 @@ def test_calculating_json():
     current_directory = os.path.dirname(__file__)
     path1 = os.path.join(current_directory, 'fixtures', "file1.json")
     path2 = os.path.join(current_directory, 'fixtures', "file2.json")
-    result = start_calculate(path1, path2, format_type='json')
+    result = generate_diff(path1, path2, format_type='json')
     etalon_path = os.path.join(
         current_directory, 'fixtures', 'etalon_test_calculating_json.json'
     )
